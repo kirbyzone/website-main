677cd34089d8b7d8003bf2077fa512d189100876
Igor Couto <igor@kirby.zone>
2021-09-24 06:48:02 +0000

    add hero banner - home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,12 @@ module.exports =
       fontFamily: {
         cursive: ["Damion", "cursive"],
         sans: ["Fira Sans", ...defaultTheme.fontFamily.sans]
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+        'spin-xslow': 'spin 16s linear infinite',
+        'spin-2xslow': 'spin 30s linear infinite',
+        'spin-3xslow': 'spin 60s linear infinite'
       }
     }
   },
