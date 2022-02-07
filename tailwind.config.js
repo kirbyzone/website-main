4cd95fa2b0575d610f5dd18a8bd709482d9dd9e6
Igor Couto <igor@kirby.zone>
2022-02-08 10:50:00 +1100

    upgrade Tailwind to version 3.0.19

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,10 @@
-// This file controls how Tailwind processes your CSS. For details, see
-// https://tailwindcss.com/docs/configuration
-const defaultTheme = require("tailwindcss/defaultTheme");
-module.exports =
-{
-  // Turning on the just-in-time compiler:
-  mode: 'jit',
+const defaultTheme = require('tailwindcss/defaultTheme')
 
-  // WARNING: CodeKit overwrites all properties of the "purge" object (except those below) with values from the UI.
-  // Visit [Project Settings > Tools > PurgeCSS] to specify content and options. The values below can be
-  // uncommented and edited if needed; all others are controlled by CodeKit.
-  //
-  // purge: {
-  //   preserveHtmlElements: true,
-  //   layers: ['base', 'components', 'utilities']
-  // },
-
-
-  //
-  // All other TailwindCSS options are 100% under your control. Edit this config file as shown in the Tailwind Docs
-  // to enable the settings or customizations you need.
-  //
+module.exports = {
+  content: [
+    "./site/templates/**/*.php",
+    "./site/snippets/**/*.php"
+  ],
   theme: {
     extend: {
       colors: {
@@ -44,17 +29,9 @@ module.exports =
         roman: 'lower-roman',
         alpha: 'lower-alpha'
       }
-    }
+    },
   },
-
-  variants: {},
-
-  //
-  // If you want to run any Tailwind plugins (such as 'tailwindcss-typography'), simply install those into the Project via the
-  // Packages area in CodeKit, then pass their names (and, optionally, any configuration values) here.
-  // Full file paths are not necessary; CodeKit will find them.
-  //
   plugins: [
     require('@tailwindcss/forms')
-  ]
+  ],
 }
