e128fb386e7268feff2039408457b1e08787e351
Daryl Cabanting <dcabz@Daryls-MacBook-Air.local>
2022-02-17 12:37:29 +0800

    update for the subscition to sendfox

--- site/snippets/subscribes/forms.php
+++ site/snippets/subscribes/forms.php
@@ -1,6 +1,13 @@
-  <form class="flex flex-col space-y-2 mt-2">
+  <!-- <form hx-post="<?=site()->url()?>/formhandler" method="POST" class="flex flex-col space-y-2 mt-2"> -->
+  <form hx-post="<?=site()->url()?>/formhandler" hx-target='.form-subscription-section' hx-swap="outerHTML" class="form-subscription-section" method="POST" class="flex flex-col space-y-2 mt-2">
+ <?php 
+        if(isset($result) ): ?>
+          <h4> <?=$result['msg']?> </h4>
+  <?php endif; ?>
     <input class="formfield" type="text" id="firstname" name="firstname" placeholder="My First Name">
     <input class="formfield" type="text" id="lastname" name="lastname" placeholder="My Last Name">
     <input class="formfield !mb-2" type="email" id="email" name="email" placeholder="My Email" required>
+    <input type="hidden" name="csrf" value="<?= csrf() ?>">
+    <input type="hidden" name="website" value="">
     <button type="submit" class="bg-cream/80 text-wine text-sm font-bold py-3 px-10 rounded-lg w-32 transition-all duration-300 hover:bg-cream">sign up</button>
   </form>
--- site/snippets/subscribes/subscribe.php
+++ site/snippets/subscribes/subscribe.php
@@ -1,17 +1,17 @@
     <!-- SUBSCRIBE SECTION -->
 <?php 
 
-  if($pg = page('global/subscription')): ?>
-    <section id="subscribe">
+  if($pg = page('global/subscription')):  ?>
+    <section id="subscribe" >
       <div class="container m-auto grid grid-cols-1 sm:grid-cols-2 gap-8 justify-center p-12">
         <div class="justify-self-center sm:justify-self-end w-full max-w-xs relative">
           <img src="<?=asset('assets/img/kirbyzone-shape-white.svg')->url()?>" alt="kirbyzone hexagon" class="w-48 absolute -top-24 -left-32 opacity-5 animate-spin-3xslow delay-500 z-0 pointer-events-none">
           <!-- TODO: form processing and feedback -->
-          <?php snippet('subscribes/forms'); ?>
+          <?php snippet('subscribes/forms',['pg'=>$pg]); ?>
         </div>
         <div class="max-w-sm justify-self-center sm:justify-self-start relative order-first sm:order-last">
           <img src="<?=asset('assets/img/kirbyzone-shape-white.svg')->url()?>" alt="kirbyzone hexagon" class="w-48 absolute top-4 -right-16 opacity-5 animate-spin-3xslow delay-500 z-0 pointer-events-none ">
-          <h2 class="text-3xl font-bold"><?=$pg->heading()?></h2>
+          <h2 class="text-3xl font-bold"><?=$pg->title()?></h2>
           <p><?=$pg->intro_text()?></p>
           <p class="text-xs opacity-70 mt-2"><?=$pg->sub_intro_text()?></p>
         </div>
