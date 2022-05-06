6416275ffe487bb8eb432309004d17646a727e6d
Igor Couto <igor@kirby.zone>
2022-05-06 10:19:32 +1000

    fix overflowing animation in subscribe section

--- site/snippets/subscribes/subscribe.php
+++ site/snippets/subscribes/subscribe.php
@@ -5,7 +5,7 @@
     <section id="subscribe" class="max-w-[100vw] overflow-x-hidden relative">
       <div class="container m-auto grid grid-cols-1 sm:grid-cols-2 gap-8 justify-center p-12">
         <div class="justify-self-center sm:justify-self-end w-full max-w-xs relative">
-          <img src="<?=asset('assets/img/kirbyzone-shape-white.svg')->url()?>" alt="kirbyzone hexagon" class="w-48 absolute -top-24 -left-32 opacity-5 animate-spin-3xslow delay-500 z-0 pointer-events-none">
+          <img src="<?=asset('assets/img/kirbyzone-shape-white.svg')->url()?>" alt="kirbyzone hexagon" class="w-48 absolute -top-12 -left-32 opacity-5 animate-spin-3xslow delay-500 pointer-events-none">
           <!-- TODO: form processing and feedback -->
           <?php snippet('subscribes/forms',['pg'=>$pg]); ?>
         </div>
